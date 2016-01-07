c5b8653024736b3bbac72d8b3f199bfc79cf2a5a
Liz Westberg <ewest081@gmail.com>
2016-01-06 22:12:41 -0600

    initial commit

---
+++ public/js/client.js
@@ -0,0 +1,11 @@
+/**
+ * Created by joelmiller on 1/6/16.
+ */
+
+console.log('Huzzah! We\'ve loaded our client side JS from the server');
+
+$(function(){
+	$("img").hide();
+});
+
+//$("img").hide();
